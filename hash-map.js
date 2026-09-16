14851ab76214ac07eee5ef00a6102e6d248e9d5c
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-09-16 14:49:17 +0100

    Add values, entries and grow methods

--- hash-map.js
+++ hash-map.js
@@ -27,6 +27,11 @@ class HashMap {
       }
     }
     bucket.push([key, value]);
+
+    let length = this.length();
+    if (length > this.loadFactor * this.capacity) {
+      this.grow();
+    }
   }
 
   get(key) {
@@ -88,6 +93,57 @@ class HashMap {
     }
     return keys;
   }
+
+  values() {
+    let values = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let j = 0; j < this.buckets[i].length; j++) {
+        values.push(this.buckets[i][j][1]);
+      }
+    }
+    return values;
+  }
+
+  entries() {
+    let array = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let j = 0; j < this.buckets[i].length; j++) {
+        array.push(this.buckets[i][j]);
+      }
+    }
+    return array;
+  }
+
+  grow() {
+    let entries = this.entries();
+    this.capacity *= 2;
+    this.buckets = Array.from(new Array(this.capacity), () => []);
+
+    for (let i = 0; i < entries.length; i++) {
+      this.set(entries[i][0], entries[i][1]);
+    }
+  }
 }
 
 const test = new HashMap();
+
+test.set('apple', 'red')
+test.set('banana', 'yellow')
+test.set('carrot', 'orange')
+test.set('dog', 'brown')
+test.set('elephant', 'gray')
+test.set('frog', 'green')
+test.set('grape', 'purple')
+test.set('hat', 'black')
+test.set('ice cream', 'white')
+test.set('jacket', 'blue')
+test.set('kite', 'pink')
+test.set('lion', 'golden')
+
+
+test.set("apple", "green")
+test.set('moon', 'silver')
+console.log(test.capacity);
+console.log(test.length());
+console.log(test.entries());
+
