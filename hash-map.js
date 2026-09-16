4822ff621a6459885f5d8e166de42905f1cee40a
victoria_mali <153543182+victoria-mali@users.noreply.github.com>
2026-09-16 15:57:23 +0100

    Create HashSet class storing only keys

--- hash-map.js
+++ hash-map.js
@@ -125,25 +125,99 @@ class HashMap {
   }
 }
 
-const test = new HashMap();
 
-test.set('apple', 'red')
-test.set('banana', 'yellow')
-test.set('carrot', 'orange')
-test.set('dog', 'brown')
-test.set('elephant', 'gray')
-test.set('frog', 'green')
-test.set('grape', 'purple')
-test.set('hat', 'black')
-test.set('ice cream', 'white')
-test.set('jacket', 'blue')
-test.set('kite', 'pink')
-test.set('lion', 'golden')
+class HashSet {
+  constructor() {
+    this.loadFactor = 0.75;
+    this.capacity = 16;
+    this.buckets = Array.from(new Array(this.capacity), () => []);
+  }
 
+  hash(key) {
+    let hashCode = 0;
 
-test.set("apple", "green")
-test.set('moon', 'silver')
-console.log(test.capacity);
-console.log(test.length());
-console.log(test.entries());
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
+    }
+
+    return hashCode;
+  }
+
+  set(key) {
+    let index = this.hash(key);
+    let bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] === key) {
+        return;
+      }
+    }
+    bucket.push(key);
+
+    let length = this.length();
+    if (length > this.loadFactor * this.capacity) {
+      this.grow();
+    }
+  }
+
+  has(key) {
+    let index = this.hash(key);
+    let bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] === key) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  remove(key) {
+    let index = this.hash(key);
+    let bucket = this.buckets[index];
+
+    for (let i = 0; i < bucket.length; i++) {
+      if (bucket[i] === key) {
+        bucket.splice(i, 1);
+        return true;
+      }
+    }
+    return false;
+  }
+
+  length() {
+    let sum = 0;
+    for (let i = 0; i < this.buckets.length; i++) {
+      sum += this.buckets[i].length;
+    }
+    return sum;
+  }
+
+  clear() {
+    this.capacity = 16;
+    this.buckets = Array.from(new Array(this.capacity), () => []);
+  }
+
+  entries() {
+    let array = [];
+    for (let i = 0; i < this.buckets.length; i++) {
+      for (let j = 0; j < this.buckets[i].length; j++) {
+        array.push(this.buckets[i][j]);
+      }
+    }
+    return array;
+  }
+
+  grow() {
+    let entries = this.entries();
+    this.capacity *= 2;
+    this.buckets = Array.from(new Array(this.capacity), () => []);
+
+    for (let i = 0; i < entries.length; i++) {
+      this.set(entries[i]);
+    }
+  }
+}
+
 
